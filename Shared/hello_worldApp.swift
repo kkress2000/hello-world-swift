e50497ebc72926c1ff137f7689086eba41024a08
Ken Kress <ken@kress.org>
2022-02-07 13:14:04 -0500

    Initial Commit

---
+++ Shared/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  Shared
+//
+//  Created by Ken on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
